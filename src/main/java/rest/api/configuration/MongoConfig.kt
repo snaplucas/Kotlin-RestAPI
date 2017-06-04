0e8598ac1689ec2bca4d230a7e80ca854099f69f
snaplucas <lucas.chapola@gmail.com>
2017-06-04 17:27:26 -0300

    +removing _class from mongo documents

--- src/main/java/rest/api/configuration/MongoConfig.kt
+++ src/main/java/rest/api/configuration/MongoConfig.kt
@@ -3,6 +3,10 @@ package rest.api.configuration
 import com.mongodb.MongoClient
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 
 @Configuration
@@ -11,4 +15,10 @@ open class MongoConfig : AbstractMongoConfiguration() {
     override fun getDatabaseName() = "test"
 
     override fun mongo() = MongoClient("127.0.0.1", 27017)
+
+    override fun mongoTemplate(): MongoTemplate {
+        val converter = MappingMongoConverter(mongoDbFactory(), MongoMappingContext())
+        converter.typeMapper = DefaultMongoTypeMapper(null)
+        return MongoTemplate(mongoDbFactory(), converter)
+    }
 }

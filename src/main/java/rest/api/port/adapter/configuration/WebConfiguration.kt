1ccfa746cbee494a3470b73cb9121d1503f9a729
snaplucas <lucas.chapola@gmail.com>
2017-06-13 19:57:29 -0300

    +WebConfiguration: swagger registry

--- src/main/java/rest/api/port/adapter/configuration/WebConfiguration.kt
+++ src/main/java/rest/api/port/adapter/configuration/WebConfiguration.kt
@@ -3,7 +3,9 @@ package rest.api.port.adapter.configuration
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @Configuration
 @EnableWebMvc
@@ -11,5 +13,9 @@ open class WebConfiguration : WebMvcConfigurerAdapter() {
 
     override fun addCorsMappings(registry: CorsRegistry?) {
         registry!!.addMapping("/**")
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry?) {
+        registry!!.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/")
     }
 }

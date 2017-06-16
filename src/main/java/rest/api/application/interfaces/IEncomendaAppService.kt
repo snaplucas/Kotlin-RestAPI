3578a7b892cac4ce250d5a6d824b606e2f2c1964
snaplucas <lucas.chapola@gmail.com>
2017-06-15 22:11:48 -0300

    +EncomendaAppService

---
+++ src/main/java/rest/api/application/interfaces/IEncomendaAppService.kt
@@ -0,0 +1,8 @@
+package rest.api.application.interfaces
+
+import rest.api.domain.model.entities.Encomenda
+
+interface IEncomendaAppService {
+
+    fun enviarEncomenda(encomenda: Encomenda, destino: String): Unit
+}
--- src/main/java/rest/api/application/services/EncomendaAppService.kt
+++ src/main/java/rest/api/application/services/EncomendaAppService.kt
@@ -1,4 +1,13 @@
 package rest.api.application.services
 
-class EncomendaAppService {
+import rest.api.application.interfaces.IEncomendaAppService
+import rest.api.application.interfaces.IEnviaEncomenda
+import rest.api.domain.model.entities.Encomenda
+
+class EncomendaAppService(val envia: IEnviaEncomenda) : IEncomendaAppService {
+
+    override fun enviarEncomenda(encomenda: Encomenda, destino: String) {
+        // TODO: validações para encomenda
+        envia.enviarEncomenda(queue = destino, mensagem = encomenda)
+    }
 }

01f32e8c43de6f02938624021238e765e39c9673
snaplucas <lucas.chapola@gmail.com>
2017-07-11 00:32:36 -0300

    Main: GsonBuilder alternative

--- src/main/java/rest/api/port/adapter/Main.kt
+++ src/main/java/rest/api/port/adapter/Main.kt
@@ -1,69 +1,49 @@
 package rest.api.port.adapter
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import org.jetbrains.ktor.application.Application
-import org.jetbrains.ktor.application.ApplicationResponse
 import org.jetbrains.ktor.application.call
 import org.jetbrains.ktor.application.install
-import org.jetbrains.ktor.features.Compression
+import org.jetbrains.ktor.content.TextContent
 import org.jetbrains.ktor.features.DefaultHeaders
-import org.jetbrains.ktor.gson.GsonSupport
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.jetty.Jetty
 import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.netty.Netty
 import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
-import org.jetbrains.ktor.routing.routing
 import rest.api.domain.model.entities.Produto
 import rest.api.domain.model.entities.Usuario
 
 data class Banda(val guitar: String, val bass: String, val drums: String)
 
 fun main(args: Array<String>) {
-    val usuario = Usuario("Mariana")
-
-    embeddedServer(Jetty, 8080) {
-        routing {
-            get("/usuarios") {
-                call.respond(usuario)
-            }
-            get("/produtos") {
-                call.respond(Produto(nome = "Produto", tipo = "Teste"))
-            }
-            get("/") {
-                call.respondText("""Hello, world!<br><a href="/bye">Say bye?</a>""", ContentType.Text.Html)
-            }
-            get("/band") {
-                call.respondText(Banda(guitar = "eddie", bass = "billy", drums = "mike").toString())
-            }
-        }
-    }.start(wait = true)
+    embeddedServer(Jetty, 8080, module = Application::module).start()
 }
 
-data class Model(val name: String, val items: List<Item>)
-data class Item(val key: String, val value: String)
+val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
-fun Application.main() {
+fun Application.module() {
     install(DefaultHeaders)
-    install(Compression)
     install(CallLogging)
-    install(GsonSupport) {
-        setPrettyPrinting()
-    }
-
-    val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
-    routing {
-        get("/v1") {
-            call.respond(model)
+    install(Routing) {
+        get("/usuarios") {
+            val usuario = Usuario(nome = "Mariana")
+            val content = TextContent(gson.toJson(usuario), ContentType.Application.Json)
+            call.respond(content)
         }
-        get("/v1/item/{key}") {
-            val item = model.items.firstOrNull { it.key == call.parameters["key"] }
-            if (item == null)
-                call.respond(HttpStatusCode.NotFound)
-            else
-                call.respond(item)
+        get("/produtos") {
+            call.respond(TextContent(gson.toJson(Produto(nome = "Produto", tipo = "Teste")), ContentType.Application.Json))
+        }
+        get("/") {
+            call.respondText("""Hello, world!<br><a href="/bye">Say bye?</a>""", ContentType.Text.Html)
+        }
+        get("/band") {
+            val band = Banda(guitar = "eddie", bass = "billy", drums = "mike")
+            val content = TextContent(gson.toJson(band), ContentType.Application.Json)
+            call.respond(content)
         }
     }
 }

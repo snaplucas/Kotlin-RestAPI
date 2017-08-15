f88c7182508d55329c2df54fef517f9110fed6fc
snaplucas <lucas.chapola@gmail.com>
2017-08-14 23:41:26 -0300

    Adding more samples and using GsonSupport

--- src/main/java/rest/api/port/adapter/Main.kt
+++ src/main/java/rest/api/port/adapter/Main.kt
@@ -5,14 +5,16 @@ package rest.api.port.adapter
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import org.jetbrains.ktor.application.Application
-import org.jetbrains.ktor.application.call
 import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.content.TextContent
+import org.jetbrains.ktor.features.CallLogging
 import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.gson.GsonSupport
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.jetty.Jetty
-import org.jetbrains.ktor.logging.CallLogging
+import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
@@ -20,6 +22,9 @@ import rest.api.domain.model.entities.Produto
 import rest.api.domain.model.entities.Usuario
 
 data class Banda(val guitar: String, val bass: String, val drums: String)
+data class Item(val key: String, val value: String)
+data class Model(val name: String, val items: List<Item>)
+
 
 fun main(args: Array<String>) {
     embeddedServer(Jetty, 8080, module = Application::module).start()
@@ -30,6 +35,7 @@ val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(GsonSupport)
     install(Routing) {
         get("/usuarios") {
             val usuario = Usuario(nome = "Mariana")
@@ -44,8 +50,15 @@ fun Application.module() {
         }
         get("/band") {
             val band = Banda(guitar = "eddie", bass = "billy", drums = "mike")
-            val content = TextContent(gson.toJson(band), ContentType.Application.Json)
-            call.respond(content)
+            call.respond(band)
+        }
+        val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+        get("/v1") {
+            call.respond(model)
+        }
+        get("/v1/item/{key}") {
+            val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+            if (item == null) call.respond(HttpStatusCode.NotFound) else call.respond(item)
         }
     }
 }
